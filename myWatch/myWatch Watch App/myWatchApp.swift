9c0c692a92ac66316796587190d7886406ce0b74
Rhoyo <ryanhkho@gmail.com>
2023-10-28 21:57:52 -1000

    initialize watch

---
+++ myWatch/myWatch Watch App/myWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  myWatchApp.swift
+//  myWatch Watch App
+//
+//  Created by Ryan Ho on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct myWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
